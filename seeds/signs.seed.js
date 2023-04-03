8d85c4e7ebe89d023b17dfb3fbbf018ccc8e2c55
pianattero <mariapianattero@gmail.com>
2023-04-03 19:27:17 +0200

    feat: added compatibility model and seeds

--- seeds/signs.seed.js
+++ seeds/signs.seed.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 require("../config/db.config");
 
 const mongoose = require("mongoose");
+
 const Sign = require("../models/Sign.model");
+const Compatibility = require("../models/Compatibility.model");
 
 const signs = require("../signs.json");
+const compatibilities = require("../compatibility.json");
 
 mongoose.connection.once("open", () => {
   console.info(
@@ -19,7 +22,19 @@ mongoose.connection.once("open", () => {
       return Sign.create(signs);
     })
     .then((createdSigns) => {
-      console.log("Signs created on DB");
+      const mappedCompatibilities = compatibilities.map((compatibility) => {
+        return {
+          ...compatibility,
+          signs: compatibility.signs.map((sign) => {
+            return createdSigns.find((createdSign) => createdSign.name === sign)
+              .id;
+          }),
+        };
+      });
+      return Compatibility.create(mappedCompatibilities);
+    })
+    .then((createdCompatibilities) => {
+      console.log("Compabilities and Signs Created");
     })
     .catch((err) => console.error(err))
     .finally(() => {
